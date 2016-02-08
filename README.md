ce62299b5900ec6a3add4d5b35878525fbd05d33
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-02-08 11:40:34 +0000

    initial commit.
    build.ps1 can be used to build RedGate.ThirdParty.SharpSvn

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # RedGate.ThirdParty.SharpSvn
 A package to wrap SharpSvn x86 and x64.
+
+This package installs:
+* SharpSvn-x86 assemblies as content files into a `x86` folder which will be copied to the build output.
+* SharpSvn-x64 assemblies as content files into a `x64` folder which will be copied to the build output.
+
+## How to build
+In powershell
+```
+git clone https://github.com/red-gate/RedGate.ThirdParty.SharpSvn
+cd .\RedGate.ThirdParty.SharpSvn
+
+# Update the version to build/grab a different version of sharpvn.
+# Update the package name once SharpSvn ship another major version.
+.\build.ps1 -PackageName 'SharpSvn.1.9' -Version '1.9003.3799.86'
+```
---
+++ build.ps1
@@ -0,0 +1,52 @@
+<#
+.SYNOPSIS
+    Build the RedGate.ThirdParty.SharpSvn nuget package.
+.DESCRIPTION
+    * Download nuget.exe
+    * Install the SharpSvn official packages. (both x86 and x64 versions)
+    * nuget pack RedGate.ThirdParty.SharpSvn.nuspec.
+    * The created RedGate.ThirdParty.SharpSvn has the same version as the SharpSvn packages it was created from.
+
+    Note that this script doesn't push the package anywhere. Do it youself.
+#>
+param (
+    # The name of the SharpSVN packages to wrap. (-x86 and x64 will be appended to it)
+    # Default to SharpSvn.1.9
+    [string] $PackageName = 'SharpSvn.1.9',
+    # The version of the SharpSVN packages to wrap.
+    # Default to 1.9003.3799.86
+    [string] $Version = '1.9003.3799.86'
+)
+
+$VerbosePreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+
+if($PackageName -like '*-x86') { throw 'No -x86 suffix please.'}
+if($PackageName -like '*-x64') { throw 'No -x64 suffix please.'}
+
+try {
+    Push-Location $PSScriptRoot
+
+    if(!(Test-Path .\nuget.exe)) {
+        Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe' -OutFile .\nuget.exe
+    }
+
+    if(Test-Path .\packages) { Remove-Item .\packages -Force -Recurse -verbose }
+    if(Test-Path .\files) { Remove-Item .\files -Force -Recurse -verbose }
+    New-Item packages -ItemType Directory -verbose| Out-Null
+    New-Item files\x86 -ItemType Directory -Force -verbose| Out-Null
+    New-Item files\x64 -ItemType Directory -Force -verbose | Out-Null
+
+    .\nuget.exe install $PackageName-x86 -OutputDirectory packages -ExcludeVersion
+    .\nuget.exe install $PackageName-x64 -OutputDirectory packages -ExcludeVersion
+
+    # Move the content of the packages to fit our nuspec file.
+    # we are only interest in files in the net20\ folder.
+    Get-ChildItem .\packages\$PackageName-x86\lib\net20\* -Include '*.svn*', '*.dll' | Copy-Item -Destination .\files\x86 -verbose
+    Get-ChildItem .\packages\$PackageName-x64\lib\net20\* -Include '*.svn*', '*.dll' | Copy-Item -Destination .\files\x64 -verbose
+
+    .\nuget.exe pack RedGate.ThirdParty.SharpSVN.nuspec -Version $Version -BasePath files -verbosity detailed
+
+} finally {
+    Pop-Location
+}
